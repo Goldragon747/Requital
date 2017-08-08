2b85f572749c08faa60e2a197ae1aab9f298be1d
mbrannen747 <mbrannen@student.neumont.edu>
2017-08-08 13:35:09 -0600

    Taco

--- Requital/Requital/MainWindow.xaml.cs
+++ Requital/Requital/MainWindow.xaml.cs
@@ -26,8 +26,8 @@ namespace Requital
         int direction = 1; // NORTH = 1 EAST = 2 SOUTH = 3 WEST = 4
         System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 
-        private double x = 0;
-        private double y = 0;
+        private double x = -900;
+        private double y = -1500;
         public MainWindow()
         {
             InitializeComponent();
@@ -79,11 +79,12 @@ namespace Requital
         }
         public void BuildCanvas()
         {
-            double left = (mainCanvas.ActualWidth - sprite.ActualWidth) / 2;
-            Canvas.SetLeft(sprite, left);
+            BitmapImage rougeLogo = new BitmapImage();
+            rougeLogo.BeginInit();
+            rougeLogo.UriSource = new Uri("pack://application:,,,/Requital;component/assets/Rogue.png");
+            rougeLogo.EndInit();
+            sprite.Source = rougeLogo;
 
-            double top = (mainCanvas.ActualHeight - sprite.ActualHeight) / 2;
-            Canvas.SetTop(sprite, top);
             BitmapImage logo = new BitmapImage();
                         logo.BeginInit();
                         logo.UriSource = new Uri("pack://application:,,,/Requital;component/assets/Cave.png");
